e6f22d0d346a262921371a11b2a2862aa86a2cf6
imenmansouri93 <imennmansourii@gmail.com>
2024-01-06 21:25:28 +0100

    new

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -7,12 +7,24 @@ interface Teacher {
     [propName: string]: any;
 }
 
-const myTeacher: Teacher = {
-    firstName: 'John',
-    lastName: 'Doe',
+//const myTeacher: Teacher = {
+  //  firstName: 'John',
+  //  lastName: 'Doe',
+  // fullTimeEmployee: true,
+  //  yearsOfExperience: 5,
+  //  location: 'New York',
+  //  contract: true,
+// additionalAttribute: 'some value'
+//};
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const director: Directors = {
+    firstName: 'Jane',
+    lastName: 'Smith',
     fullTimeEmployee: true,
-    yearsOfExperience: 5,
-    location: 'New York',
-    contract: true,
-    additionalAttribute: 'some value'
+    location: 'Los Angeles',
+    numberOfReports: 10,
   };
